87ed2ee1bb7c577fd270866e5d1637fcdc0d489f
SadkiratSingh <sadkiratsingh@yahoo.com>
2020-05-24 14:03:43 +0530

    auth added

--- student_db.py
+++ student_db.py
@@ -1,14 +1,76 @@
 from tkinter import *
 from tkinter import ttk
+import mysql.connector
+from mysql.connector import Error
 
 class StudentDB:
     headers=['ID','First Name','Last Name','Email','Street','City','State',
              'Zip','Phone','DOB','Sex','Lunch']
     student_info=[(1,'Harry', 'Truman', 'htruman@aol.com', '202 South St', 'Vancouver', 'WA', 98660,          '792-223-9810', '1946-1-24','M',3.50),(2,'Shelly', 'Johnson', 'sjohnson@aol.com', '9 Pond Rd', 'Sparks', 'NV', 89431, '792-223-6734', '1970-12-12','F', 3.50),(3,'Bobby', 'Briggs', 'bbriggs@aol.com', '14 12th St', 'San Diego', 'CA',92101, '792-223-6178', '1967-5-24','M', 3.50),(4,'Donna', 'Hayward', 'dhayward@aol.com', '120 16th St', 'Davenport', 'IA', 52801, '792-223-2001', '1970-3-24','F',3.50),(5,'Audrey', 'Horne', 'ahorne@aol.com', '342 19th St', 'Detroit', 'MI', 48222, '792-223-2001', '1965-2-1','F', 3.50),(6,'James', 'Hurley', 'jhurley@aol.com', '2578 Cliff St', 'Queens', 'NY', 11427, '792-223-1890', '1967-1-2','M',3.50)]
 
+    conn=None
+    cursor=None
+    query=None
+
     def __init__(self):
         self.tree=None
-        self.create_widgets()
+        self.username=StringVar(root,value='')
+        self.password=StringVar(root,value='')
+        self.create_login_db()
+
+    def create_login_db(self):
+        root.title("Login")
+        root.geometry('250x150')
+        self.frame=Frame(root,width=250,height=150)
+        self.frame.columnconfigure(0,weight=1)
+        self.frame.columnconfigure(1,weight=1)
+        self.frame.pack()
+
+        #username#
+        user_label=Label(self.frame,text='Username:')
+        user_label.grid(row=0,column=0,sticky='E',padx=5,pady=10)
+        user_entry=Entry(self.frame,textvariable=self.username)
+        user_entry.grid(row=0,column=1,sticky='W',padx=5,pady=10)
+        #username#
+    
+        #password#
+        pass_label=Label(self.frame,text='Password:')
+        pass_label.grid(row=1,column=0,sticky='E',padx=5,pady=10)
+        pass_entry=Entry(self.frame,textvariable=self.password,show='*')
+        pass_entry.grid(row=1,column=1,sticky='W',padx=5,pady=10)
+        #password#
+
+        #button#
+        login_button=Button(self.frame,text='login',command=self.login_and_connect)
+        login_button.grid(row=2,column=0,padx=5,pady=10,columnspan=2)
+        #button#
+
+        #error_label#
+        self.error_var=StringVar(root,value='')
+        error_label=Label(self.frame,textvariable=self.error_var)
+        error_label.grid(row=3,column=0,columnspan=2)
+        #error_label#
+
+    def login_and_connect(self):
+        #verify#
+        if(len(self.username.get())==0 or
+           self.username.get()!='studentadmin' or
+           len(self.password.get())==0 or
+           self.password.get()!='khalsapanth1*'):
+           self.error_var.set('Enter correct credentials!')
+        #verify
+
+        #now login#
+        else:
+            self.delete_login_widgets()
+            root.geometry("1500x600")
+            root.title("Manage Student Records")
+            self.create_widgets()
+
+    def delete_login_widgets(self):
+        for w in self.frame.grid_slaves():
+            w.destroy()
+        self.frame.destroy()
 
     def create_widgets(self):
         #----ROW1-----#
@@ -136,7 +198,6 @@ class StudentDB:
             self.tree.insert('','end',values=data)
 
         self.tree.grid(row=3,column=0,columnspan=20)
-
         #treeview#
 
     def add_student(self):
@@ -153,7 +214,7 @@ class StudentDB:
 
 
 root=Tk()
-root.geometry('1500x600')
-root.title("StudentRecords")
+#root.geometry('1500x600')
+#root.title("StudentRecords")
 stu_db=StudentDB()
 root.mainloop()
